2b8696ba3870c0d5f338e84871753cf808406ce0
Nishant Kumar <nishant@Nishants-MacBook-Air.local>
2023-07-10 03:03:19 +0530

    restructure

--- src/design-patterns/adapter/index.ts
+++ src/design-patterns/adapter/index.ts
@@ -1,4 +1,4 @@
-import VechileAdapter from "./object/adatper";
+import VechileAdapter from "./src/adatper";
 
 const apache_rtr_180 = VechileAdapter('bike' ,{weight : 160, wheels : 2, abs : true, gears : 5, clutch : true, maxSpeed : 180});
 const skoda_kushaq = VechileAdapter('car' ,{weight : 1200, wheels : 4, powerStearing : true, gears : 5, clutch : true, maxSpeed : 185});
